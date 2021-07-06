9eaad1e2845f1e6a6b183edf35d3f0f8a0b73b24
jbtobar <jbtobar.io@gmail.com>
2021-07-06 13:29:50 -0500

    renevew frst commit

---
+++ sql.sql
@@ -0,0 +1,22 @@
+create table ipf_opt (
+  TYPE varchar,
+  SYMBOL varchar,
+  COUNTRY varchar,
+  OPOL varchar,
+  EXCHANGES varchar,
+  CURRENCY varchar,
+  CFI varchar,
+  MULTIPLIER varchar,
+  UNDERLYING varchar,
+  SPC varchar,
+  ADDITIONAL_UNDERLYINGS varchar,
+  MMY varchar,
+  EXPIRATION varchar,
+  LAST_TRADE varchar,
+  STRIKE varchar,
+  OPTION_TYPE varchar,
+  EXPIRATION_STYLE varchar,
+  SETTLEMENT_STYLE varchar,
+  PRICE_INCREMENTS varchar,
+  TRADING_HOURS varchar
+)
---
+++ src/getAllContracts.js
@@ -0,0 +1,116 @@
+require('dotenv').config()
+const { parse } = require('@fast-csv/parse');
+const { Pool } = require('pg')
+const copyTo = require('pg-copy-streams').to
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  // host: process.env.HOST_EXT,
+  database: process.env.DB_DB,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+})
+
+
+
+
+// https://tools.dxfeed.com/ipf?
+//
+//
+
+
+
+let rowLength = 0
+
+pool.connect(async function (err, client, done) {
+  const streamQ = client.query(copyFrom("COPY ipf_opt FROM STDIN WITH CSV DELIMITER ',' QUOTE '\"'"))
+  const readable = new Stream.Readable()
+
+
+
+
+
+
+
+
+
+
+
+
+  const stream = parse({ headers: true })
+    .on('error', error => console.error(error))
+    .on('data', row => {
+      rowLength+=1
+      const json = JSON.stringify(row)
+      readable.push(json.substring(1, json.length-1)+'\n')
+    })
+    .on('end', rowCount => {
+      console.log(`Parsed ${rowCount} rows`)
+      readable.push(null)
+      // console.log('finished pushing')
+      streamQ.on('error', (err) => console.log('streamerr',err))
+      // stream.on('finish', console.log)
+      readable.pipe(streamQ)
+    });
+
+
+  const https = require('https')
+  const options = {
+    hostname: 'tools.dxfeed.com',
+    port: 443,
+    path: '/ipf?types=OPTION',
+    method: 'GET',
+    auth: `${process.env.DXUSER}:${process.env.CODE}`
+  }
+  // console.log({options})
+  const req = https.request(options, res => {
+    console.log(`statusCode: ${res.statusCode}`)
+
+    res.on('data', d => {
+      // process.stdout.write(d)
+      // process.exit()
+      stream.write(d);
+
+    })
+    res.on('end',() => {
+      console.log('ended',rowLength)
+      stream.end();
+      setTimeout(() => {
+        console.log('png',rowLength)
+      },3000)
+    })
+  })
+
+  req.on('error', error => {
+    console.error(error)
+  })
+
+  req.end()
+
+
+
+})
+
+
+
+
+// #ETF::=TYPE,SYMBOL,DESCRIPTION,COUNTRY,OPOL,EXCHANGES,CURRENCY,PRICE_INCREMENTS,TRADING_HOURS,SUBTYPES
+// #OPTION::=TYPE,SYMBOL,COUNTRY,OPOL,EXCHANGES,CURRENCY,CFI,MULTIPLIER,UNDERLYING,SPC,ADDITIONAL_UNDERLYINGS,MMY,EXPIRATION,LAST_TRADE,STRIKE,OPTION_TYPE,EXPIRATION_STYLE,SETTLEMENT_STYLE,PRICE_INCREMENTS,TRADING_HOURS
+// #STOCK::
+
+///
+//
+// var http = require('http');
+//
+// var request = http.request({'hostname': 'www.example.com',
+//                             'auth': 'user:password'
+//                            },
+//                            function (response) {
+//                              console.log('STATUS: ' + response.statusCode);
+//                              console.log('HEADERS: ' + JSON.stringify(response.headers));
+//                              response.setEncoding('utf8');
+//                              response.on('data', function (chunk) {
+//                                console.log('BODY: ' + chunk);
+//                              });
+//                            });
+// request.end();
