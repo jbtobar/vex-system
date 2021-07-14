e45e59d3542f8c2e258e6e413003e4e81a587623
jbtobar <jbtobar.io@gmail.com>
2021-07-13 19:46:40 -0500

    comet full

--- oimon.js
+++ oimon.js
@@ -3,7 +3,7 @@ const client = redis.createClient()
 const { timenow } = require('./utils');
 
 const checkStatus = () => {
-  client.hmget(['.TSLA210723C1100','openInterest'],(err,res) => {
+  client.hmget(['.TSLA210723C700','openInterest'],(err,res) => {
     console.log(`${Number(res[0])} - ${timenow()}`)
   })
 }
