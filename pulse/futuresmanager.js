f759cb338054332da9310dc7e9d23c3674e7ad75
jbtobar <jbtobar.io@gmail.com>
2021-07-15 16:48:44 -0500

    futuresmanger

---
+++ pulse/futuresmanager.js
@@ -0,0 +1,10 @@
+/**
+ * Code written by and belongs to Juan Bernardo Tobar <jbtobar>
+ * jbtobar.io@gmail.com
+ * @author Juan Bernardo Tobar <jbtobar.io@gmail.com>
+ */
+const GET_ALL_FUTURES_AND_OPTIONS = require('../src/GET_ALL_FUTURES_AND_OPTIONS');
+
+(async () => {
+  await GET_ALL_FUTURES_AND_OPTIONS()
+})()
