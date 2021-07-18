c7e3004ac0e63c0bb3007a12afb0038947cb9375
jbtobar <jbtobar.io@gmail.com>
2021-07-18 18:34:49 -0500

    3pm

--- pulse/publisherTASFUT.js
+++ pulse/publisherTASFUT.js
@@ -23,7 +23,8 @@ const get = (args) => {
   });
 }
 const yesterday5PM = () => Math.floor(new Date().getTime()/8.64e7)*8.64e7+ 1000*60*60*4
-const today545pm = () => Math.floor(new Date().getTime()/8.64e7)*8.64e7+(1000*60*60*22+1000*60*45)
+// const today545pm = () => Math.floor(new Date().getTime()/8.64e7)*8.64e7+(1000*60*60*22+1000*60*45)
+const today300pm = () => Math.floor(new Date().getTime()/8.64e7)*8.64e7+(1000*60*60*20)
 // new Date(
 //   Math.floor(new Date().getTime()/8.64e7)*8.64e7+ 1000*60*60*4
 // ).toLocaleString("en-US", {timeZone: "America/New_York"})
@@ -32,7 +33,8 @@ let timeId = 0;
 (async () => {
   const lastFut = await get('LAST_FUT')
   console.log(lastFut)
-  timeId = today545pm()
+  // timeId = today545pm()
+  timeId = today300pm()
   console.log('firsttime',{timeId})
   // process.exit()
 })();
