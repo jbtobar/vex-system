64a0a04c3b742cf7266710aadc7eb88401d624ec
jbtobar <jbtobar.io@gmail.com>
2021-07-19 17:26:23 -0500

    machin

--- pulse/machine.js
+++ pulse/machine.js
@@ -7,41 +7,50 @@
 // const exec = util.promisify(require('child_process').exec);
 const childProcess = require('child_process');
 
-function runScript(scriptPath, callback) {
+const run = (scriptPath) => {
+  return new Promise((resolve, reject) => {
+    let invoked = false;
 
-    // keep track of whether callback has been invoked to prevent multiple invocations
-    var invoked = false;
-
-    var process = childProcess.fork(scriptPath);
+    const process = childProcess.fork(scriptPath);
 
     // listen for errors as they may prevent the exit event from firing
-    process.on('error', function (err) {
+    process.on('error',  (err) => {
         if (invoked) return;
         invoked = true;
-        callback(err);
+        // callback(err);
+        reject(err)
     });
 
     // execute the callback once the process has finished running
-    process.on('exit', function (code) {
+    process.on('exit',  (code) =>  {
         if (invoked) return;
         invoked = true;
         var err = code === 0 ? null : new Error('exit code ' + code);
-        callback(err);
+        if (err) reject(err)
+        else resolve(code)
+        // callback(err);
     });
-
+  });
 }
 
-// Now we can run a script and invoke a callback when complete, e.g.
-runScript('pulse/test.js', function (err) {
-    if (err) throw err;
-    console.log('finished running some-script.js');
-});
+// function runScript(scriptPath, callback) {
+//
+//     // keep track of whether callback has been invoked to prevent multiple invocations
+//
+//
+// }
+//
+// // Now we can run a script and invoke a callback when complete, e.g.
+// runScript('pulse/test.js', function (err) {
+//     if (err) throw err;
+//     console.log('finished running some-script.js');
+// });
 
 
-// (async () => {
-//   const m = await exec(`node pulse/test.js`)
-//   console.log(m)
-// })();
+(async () => {
+  const m = await run(`pulse/test.js`)
+  console.log(m)
+})();
 // const futuresMarketOpen = async () => {
 //   `pm2 start`
 //   await resetToken()
