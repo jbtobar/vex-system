6e8a4c489a36b473b5106d7c0ef1c649ab4c6815
jbtobar <jbtobar.io@gmail.com>
2021-07-20 06:56:57 -0500

    test

--- pulse/cometTASFUT.js
+++ pulse/cometTASFUT.js
@@ -95,7 +95,7 @@ pool.connect(async function (err, client, done) {
              readable._read = () => {}
              while (payload.length > 0) {
                const singleLoad = payload.splice(0,CHANNEL_DATA_LENGTHS['TimeAndSale'])
-               console.log(singleLoad)
+               // console.log(singleLoad)
                if (singleLoad[13] === '\x00') singleLoad[13] =  0
                if (singleLoad[7] === '\u0000') singleLoad[7] =  0
 
@@ -124,7 +124,7 @@ pool.connect(async function (err, client, done) {
                  singleLoad[20],
                  ...context
                ])
-               console.log(json)
+               // console.log(json)
                client_redis.set('LAST_FUT',singleLoad[3])
                client_redis.publish('TASERFUT',json)
                readable.push(json.substring(1, json.length-1)+'\n')
