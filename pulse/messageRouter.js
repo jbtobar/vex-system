51165ff202618dbc0a290b54047b637e8c73ef1a
jbtobar <jbtobar.io@gmail.com>
2021-07-22 18:53:18 -0500

    messageRouter

---
+++ pulse/messageRouter.js
@@ -0,0 +1,29 @@
+const redis = require('redis');
+
+const subber = redis.createClient();
+const publisher = redis.createClient();
+
+
+subber.on('message', (channel, message) => {
+  try {
+    const payload = JSON.parse(message)
+    switch (channel) {
+      case 'TASER':
+      case 'TASERFUT':
+        publisher.publish(`${payload[0]}::TAS`,message)
+        break;
+      default:
+        publisher.publish(`${payload.eventSymbol}::${channel}`,message)
+    }
+  } catch (e) {
+    console.error(e)
+  }
+})
+subber.subscribe('TASER')
+subber.subscribe('TASERFUT')
+subber.subscribe('Trade')
+subber.subscribe('Greeks')
+subber.subscribe('Summary')
+subber.subscribe('Quote')
+subber.subscribe('Underlying')
+subber.subscribe('Series')
