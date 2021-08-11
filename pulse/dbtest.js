026b67a6f36e1e64f327e30598e34bdf3fcb028a
jbtobar <jbtobar.io@gmail.com>
2021-08-11 09:28:56 -0500

    countDecimals

--- pulse/dbtest.js
+++ pulse/dbtest.js
@@ -148,8 +148,8 @@ const fixNum = (val) => {
     // console.log(val,Number(val.toFixed(6)))
     return Number(num.toFixed(6))
   }
-  if (num > 0 && num < 0.00000001) return num.toFixed(6)
-  return num
+  // if (num > 0 && num < 0.00000001) return num.toFixed(6)
+  return num.toFixed(6)
 }
 
 const cleanString = (val) => {
