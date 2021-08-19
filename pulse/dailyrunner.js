bd873149fb0287961c2a85402bde9a2ce0d9466c
jbtobar <jbtobar.io@gmail.com>
2021-08-19 06:55:48 -0500

    dailyrunne

--- pulse/dailyrunner.js
+++ pulse/dailyrunner.js
@@ -8,12 +8,42 @@ const { query } = require('../db')
 const subber = redis.createClient();
 
 (async () =>  {
-  const contracts = await query(`SELECT optioncode,openinterest - prevoi as oich from opt_db where optioncode != '';`)
+  const contracts = await query(`SELECT optioncode,openinterest - prevoi as oich,
+valuebuy,
+volmbuy,
+countbuy,
+valuesell,
+volmsell,
+countsell,
+valueund,
+volmund,
+countund,
+value,
+volm,
+count,
+valuebs,
+volmbs,
+countbs from opt_db where optioncode != '';`)
   console.log(contracts.rows.length)
   console.log(contracts.rows[0])
   for (var i = 0; i < contracts.rows.length; i++) {
     subber.hmset(contracts.rows[i].optioncode,{
-      oich:contracts.rows[i].oich  || 'null'
+      oich:contracts.rows[i].oich  || 'null',
+      valuebuy:contracts.rows[i].valuebuy  || 0,
+      volmbuy:contracts.rows[i].volmbuy  || 0,
+      countbuy:contracts.rows[i].countbuy  || 0,
+      valuesell:contracts.rows[i].valuesell  || 0,
+      volmsell:contracts.rows[i].volmsell  || 0,
+      countsell:contracts.rows[i].countsell  || 0,
+      valueund:contracts.rows[i].valueund  || 0,
+      volmund:contracts.rows[i].volmund  || 0,
+      countund:contracts.rows[i].countund  || 0,
+      value:contracts.rows[i].value  || 0,
+      volm:contracts.rows[i].volm  || 0,
+      count:contracts.rows[i].count  || 0,
+      valuebs:contracts.rows[i].valuebs  || 0,
+      volmbs:contracts.rows[i].volmbs  || 0,
+      countbs:contracts.rows[i].countbs  || 0,
     })
   }
   console.log('done')
