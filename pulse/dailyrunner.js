da02309942f48231f7fe5a7680f320eefc23a299
jbtobar <jbtobar.io@gmail.com>
2021-08-23 06:50:13 -0500

    machine

--- pulse/dailyrunner.js
+++ pulse/dailyrunner.js
@@ -179,23 +179,23 @@ const getFlowRep = async () => {
   var batch = client.batch();
   for (var i = 0; i < contracts.rows.length; i++) {
     batch.hmset(contracts.rows[i].optioncode,{
-      oich:contracts.rows[i].oich  || 'null',
-      // valuebuy:contracts.rows[i].valuebuy  || 0,
-      // volmbuy:contracts.rows[i].volmbuy  || 0,
-      // countbuy:contracts.rows[i].countbuy  || 0,
-      // valuesell:contracts.rows[i].valuesell  || 0,
-      // volmsell:contracts.rows[i].volmsell  || 0,
-      // countsell:contracts.rows[i].countsell  || 0,
-      // valueund:contracts.rows[i].valueund  || 0,
-      // volmund:contracts.rows[i].volmund  || 0,
-      // countund:contracts.rows[i].countund  || 0,
-      // value:contracts.rows[i].value  || 0,
-      // volm:contracts.rows[i].volm  || 0,
-      // count:contracts.rows[i].count  || 0,
-      // valuebs:contracts.rows[i].valuebs  || 0,
-      // volmbs:contracts.rows[i].volmbs  || 0,
-      // countbs:contracts.rows[i].countbs  || 0,
-      // underlying:contracts.rows[i].rootsymbol
+      oich:contracts.rows[i].oich  || 0,
+      valuebuy:contracts.rows[i].valuebuy  || 0,
+      volmbuy:contracts.rows[i].volmbuy  || 0,
+      countbuy:contracts.rows[i].countbuy  || 0,
+      valuesell:contracts.rows[i].valuesell  || 0,
+      volmsell:contracts.rows[i].volmsell  || 0,
+      countsell:contracts.rows[i].countsell  || 0,
+      valueund:contracts.rows[i].valueund  || 0,
+      volmund:contracts.rows[i].volmund  || 0,
+      countund:contracts.rows[i].countund  || 0,
+      value:contracts.rows[i].value  || 0,
+      volm:contracts.rows[i].volm  || 0,
+      count:contracts.rows[i].count  || 0,
+      valuebs:contracts.rows[i].valuebs  || 0,
+      volmbs:contracts.rows[i].volmbs  || 0,
+      countbs:contracts.rows[i].countbs  || 0,
+      underlying:contracts.rows[i].rootsymbol
     })
   }
   batch.exec((err, resp)=> {
