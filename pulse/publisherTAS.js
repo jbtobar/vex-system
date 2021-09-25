d5764628b600037c14fae78387b9046708fa7508
jbtobar <jbtobar.io@gmail.com>
2021-09-24 19:03:19 -0500

    checkpoint - sends pane idfA

--- pulse/publisherTAS.js
+++ pulse/publisherTAS.js
@@ -43,8 +43,7 @@ let timeId = 0;
 (async () => {
   const lastFut = await get('LAST_TAS')
   console.log(lastFut)
-  timeId =  1632499687857
-  ///todayMidnight()
+  timeId = todayMidnight()
   console.log('firsttime',{timeId})
   // process.exit()
 })();
