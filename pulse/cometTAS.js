63583f0320bb33a80d1607b60ffc431df9599401
jbtobar <jbtobar.io@gmail.com>
2021-09-28 06:42:39 -0500

    checkpoint - sends pane idfA

--- pulse/cometTAS.js
+++ pulse/cometTAS.js
@@ -155,7 +155,7 @@ pool.connect(async function (err, client, done) {
                  ])
                  client_redis.set('LAST_TAS',singleLoad[3])
                  client_redis.publish('TASER',json)
-                 readable.push(json.replace(/"null"/g, null).replace(/'null'/g, null)..substring(1, json.length-1)+'\n')
+                 readable.push(json.replace(/"null"/g, null).replace(/'null'/g, null).substring(1, json.length-1)+'\n')
                }
              }
              readable.push(null)
