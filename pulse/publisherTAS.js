930d388a37b9897e33fa07d5e4e11d833db35dee
jbtobar <jbtobar.io@gmail.com>
2021-10-04 09:19:11 -0500

    non checkpoint - convert_to_serial

--- pulse/publisherTAS.js
+++ pulse/publisherTAS.js
@@ -43,8 +43,7 @@ let timeId = 0;
 (async () => {
   const lastFut = await get('LAST_TAS')
   console.log(lastFut)
-  timeId =  1633345456045
-   //todayMidnight()
+  timeId = todayMidnight()
   console.log('firsttime',{timeId})
   // process.exit()
 })();
--- pulse/publisherTASFUT.js
+++ pulse/publisherTASFUT.js
@@ -48,8 +48,7 @@ let timeId = 0;
   console.log(lastFut)
   // timeId = today500pm()
   // timeId = today545pm()
-  timeId = 1633343311342
-  //today545pm();
+  timeId = today545pm();
   // timeId = today300pm()
   console.log('firsttime',{timeId})
   // process.exit()
