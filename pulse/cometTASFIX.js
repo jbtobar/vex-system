6abb2618c37b4f31f04e2a954fa3bd75455678ae
jbtobar <jbtobar.io@gmail.com>
2022-02-08 11:47:55 -0500

    FIX

---
+++ pulse/cometTASFIX.js
@@ -0,0 +1,213 @@
+/**
+ * Code written by and belongs to Juan Bernardo Tobar <jbtobar>
+ * jbtobar.io@gmail.com
+ * @author Juan Bernardo Tobar <jbtobar.io@gmail.com>
+ */
+const redis = require('redis');
+const connectedComet = require('../comet');
+const { SUBSCRIPTION_CHANNEL, TIME_SERIES_CHANNEL,CHANNEL_DATA_LENGTHS } = require('../comet/constants')
+const pool = require('../db').pool
+const Stream = require('stream')
+const copyFrom = require('pg-copy-streams').from
+
+const timenow = () => new Date().toLocaleString("en-US", {timeZone: "America/New_York"})
+console.log('-----------------------------------------------------------------')
+console.log('-----------------------------------------------------------------')
+console.log('-----------------------------------------------------------------')
+console.log('-----------------------------------------------------------------')
+console.log('-----------------------------------------------------------------')
+console.log(`cometTAS - start - ${timenow()} - v4.0.5`)
+console.log('-----------------------------------------------------------------')
+console.log('-----------------------------------------------------------------')
+console.log('-----------------------------------------------------------------')
+console.log('-----------------------------------------------------------------')
+console.log('-----------------------------------------------------------------')
+
+Number.prototype.countDecimals = function () {
+
+    if (Math.floor(this.valueOf()) === this.valueOf()) return 0;
+
+    var str = this.toString();
+    if (str.indexOf(".") !== -1 && str.indexOf("-") !== -1) {
+        return str.split("-")[1] || 0;
+    } else if (str.indexOf(".") !== -1) {
+        return str.split(".")[1].length || 0;
+    }
+    return str.split("-")[1] || 0;
+}
+
+const client_redis = redis.createClient();
+const publisher = redis.createClient();
+
+const LENGTHS = {
+
+}
+// const fixNum = (val) => isNaN(val) ? 0 : Number(val);
+const fixNum = (val) => {
+  if (!val) return 0
+  if (val === Infinity) return 0;
+  if (val === -Infinity) return 0;
+  if (val === null) return 0;
+  if (val === 'null') return 0;
+  if (val === false) return 0;
+  if (val === undefined) return 0;
+  if (isNaN(val)) return 0;
+  const num = Number(val)
+  if (Math.abs(Number(Math.abs(num).countDecimals())) > 6)  {
+    return Number(num.toFixed(6))
+  }
+  return num
+}
+
+const contextForContract = (ctc) => {
+  return new Promise(function(resolve, reject) {
+    // console.log({ctc});
+    client_redis.hmget([ctc,'underlying','volatility','mmy','delta','gamma','vega','theta'],(err,res) => {
+      // console.log({res});
+      if (err) console.error(err)
+      if (res[0]) {
+        client_redis.hmget([res[0],'price'],(e,r) => {
+          // console.log({r});
+          if (e) console.error(e)
+          resolve([
+            fixNum(r[0]),
+            fixNum(res[1]),
+            res[0],
+            fixNum(res[2]),
+            fixNum(res[3]),
+            fixNum(res[4]),
+            fixNum(res[5]),
+            fixNum(res[6])
+          ])
+        })
+      } else {
+        resolve([
+          fixNum(0),
+          fixNum(res[1]),
+          res[0],
+          fixNum(res[2]),
+          fixNum(res[3]),
+          fixNum(res[4]),
+          fixNum(res[5]),
+          fixNum(res[6])
+        ])
+      }
+
+    })
+  });;
+}
+
+let messageNum = 0
+let okNum = 0
+
+const processReceived = (payload) => {
+  payload.addTimeSeries.TimeAndSale.forEach((item, i) => {
+    client_redis.hmset(item.eventSymbol,{pubok:new Date().getTime()})
+  });
+}
+
+
+pool.connect(async function (err, client, done) {
+  try {
+    const handeTimeSeriesChannel = async msg => {
+      try {
+        // console.log(msg)
+        client_redis.set('cometTAS',new Date().getTime())
+        messageNum+=1;
+        const [payloadType,payload] = msg.data
+         if (payload.length > 0) {
+           if (payloadType === 'TimeAndSale' || payloadType[0] === 'TimeAndSale') {
+             // console.log('istands')
+             // const stream = client.query(copyFrom("COPY tasc FROM STDIN WITH CSV DELIMITER ',' QUOTE '\"'"))
+             // const readable = new Stream.Readable()
+             // readable._read = () => {}
+             while (payload.length > 0) {
+               const singleLoad = payload.splice(0,CHANNEL_DATA_LENGTHS['TimeAndSale'])
+               // console.log(singleLoad)
+               if (Number(singleLoad[9]) > 0 ) {
+                 if (singleLoad[13] === '\x00') singleLoad[13] =  0
+                 if (singleLoad[7] === '\u0000') singleLoad[7] =  0
+
+                 const context = await contextForContract(singleLoad[0])
+                 const json = JSON.stringify([
+                   singleLoad[0],
+                   // 'eventTime',singleLoad[1],
+                   // 'eventFlags',singleLoad[2],
+                   singleLoad[3],
+                   singleLoad[4],
+                   // 'timeNanoPart',singleLoad[5],
+                   singleLoad[6],
+                   singleLoad[7],
+                   fixNum(singleLoad[8]),
+                   fixNum(singleLoad[9]),
+                   fixNum(singleLoad[10]),
+                   fixNum(singleLoad[11]),
+                   singleLoad[12],
+                   singleLoad[13],
+                   singleLoad[14],
+                   singleLoad[15],
+                   singleLoad[16],
+                   singleLoad[17],
+                   singleLoad[18],
+                   singleLoad[19],
+                   singleLoad[20],
+                   ...context
+                 ])
+                 client_redis.set('LAST_TAS_K',singleLoad[3])
+                 client_redis.publish('TASER',json)
+                 // readable.push(json.replace(/"null"/g, null).replace(/'null'/g, null).substring(1, json.length-1)+'\n')
+               }
+             }
+             // readable.push(null)
+             // console.log('finished pushing')
+             // stream.on('error', (err) => console.log('streamerr',err,payload))
+             // stream.on('finish', console.log)
+             // readable.pipe(stream)
+           }
+         }
+      } catch (e) {
+        console.error(msg,e);
+      }
+    }
+
+    console.log('suball')
+    let startTime = new Date().getTime()
+    const comet = await connectedComet()
+
+
+
+    comet.subscribe(TIME_SERIES_CHANNEL, handeTimeSeriesChannel)
+
+    publisher.on('message', (channel, message) => {
+      const payload = JSON.parse(message)
+      // console.log(message)
+      // console.log(payload)
+      switch (channel) {
+        case 'cometTAS':
+          comet.publish(SUBSCRIPTION_CHANNEL,payload,ack => {
+            if (!ack.successful) {
+              console.log('sub fail',ack)
+            } else {
+              processReceived(payload)
+              okNum+=1
+            }
+          })
+          break;
+        default:
+      }
+    })
+    publisher.subscribe('cometTAS')
+
+
+
+
+
+
+
+    setInterval(() => {
+      console.log('cometTAS',{messageNum,okNum,time:timenow()})
+    },1000*30)
+  } catch (e) {
+    console.error(e)
+  }
+})
---
+++ pulse/dailyrunnerfixo.js
@@ -0,0 +1,210 @@
+/**
+ * Code written by and belongs to Juan Bernardo Tobar <jbtobar>
+ * jbtobar.io@gmail.com
+ * @author Juan Bernardo Tobar <jbtobar.io@gmail.com>
+ */
+// const redis = require('redis');
+const { client, get } = require('../redis');
+const { query } = require('../db')
+// const client_redis = redis.createClient();
+
+const timenow = () => new Date().toLocaleString("en-US", {timeZone: "America/New_York"})
+const getFlowRep = async () => {
+  try {
+    const timeStart = new Date().getTime()
+    console.log(`Starting - ${timenow()}`)
+    const data = {}
+    const results = await get("FLO::ALL::C")
+    const tab = JSON.parse(results)
+    data.toppremium = tab.sort((a,b) => b.value - a.value).filter((d,i) => i < 100).map(item => ({
+      symbol: item.symbol,
+      flowratio: item.flowratio,
+      volm: item.volm,
+      value: item.value,
+      bearflow: item.bearflow,
+      bullflow: item.bullflow,
+      valuecall: item.valuecall,
+      valueput: item.valueput,
+      valuebuycall: item.valuebuycall,
+      valuesellcall: item.valuesellcall,
+      valuebuyput: item.valuebuyput,
+      valuesellput: item.valuesellput
+    }))
+    data.topvolume = tab.sort((a,b) => b.volm - a.volm).filter((d,i) => i < 100).map(item => ({
+      symbol: item.symbol,
+      flowratio: item.flowratio,
+      volm: item.volm,
+      value: item.value,
+      bearflow: item.bearflow,
+      bullflow: item.bullflow,
+      valuecall: item.valuecall,
+      valueput: item.valueput,
+      valuebuycall: item.valuebuycall,
+      valuesellcall: item.valuesellcall,
+      valuebuyput: item.valuebuyput,
+      valuesellput: item.valuesellput
+    }))
+    data.topbullflow1m = tab.sort((a,b) => b.flowratio - a.flowratio).filter((d,i) => d.value > 1000000).filter((d,i) => i < 100).map(item => ({
+      symbol: item.symbol,
+      flowratio: item.flowratio,
+      volm: item.volm,
+      value: item.value,
+      bearflow: item.bearflow,
+      bullflow: item.bullflow,
+      valuecall: item.valuecall,
+      valueput: item.valueput,
+      valuebuycall: item.valuebuycall,
+      valuesellcall: item.valuesellcall,
+      valuebuyput: item.valuebuyput,
+      valuesellput: item.valuesellput
+    }))
+    data.topbullflow10m = tab.sort((a,b) => b.flowratio - a.flowratio).filter((d,i) => d.value > 10000000).filter((d,i) => i < 100).map(item => ({
+      symbol: item.symbol,
+      flowratio: item.flowratio,
+      volm: item.volm,
+      value: item.value,
+      bearflow: item.bearflow,
+      bullflow: item.bullflow,
+      valuecall: item.valuecall,
+      valueput: item.valueput,
+      valuebuycall: item.valuebuycall,
+      valuesellcall: item.valuesellcall,
+      valuebuyput: item.valuebuyput,
+      valuesellput: item.valuesellput
+    }))
+
+    data.topbearflow1m = tab.sort((a,b) => a.flowratio - b.flowratio).filter((d,i) => d.value > 1000000).filter((d,i) => i < 100).map(item => ({
+      symbol: item.symbol,
+      flowratio: item.flowratio,
+      volm: item.volm,
+      value: item.value,
+      bearflow: item.bearflow,
+      bullflow: item.bullflow,
+      valuecall: item.valuecall,
+      valueput: item.valueput,
+      valuebuycall: item.valuebuycall,
+      valuesellcall: item.valuesellcall,
+      valuebuyput: item.valuebuyput,
+      valuesellput: item.valuesellput
+    }))
+    data.topbearflow10m = tab.sort((a,b) => a.flowratio - b.flowratio).filter((d,i) => d.value > 10000000).filter((d,i) => i < 100).map(item => ({
+      symbol: item.symbol,
+      flowratio: item.flowratio,
+      volm: item.volm,
+      value: item.value,
+      bearflow: item.bearflow,
+      bullflow: item.bullflow,
+      valuecall: item.valuecall,
+      valueput: item.valueput,
+      valuebuycall: item.valuebuycall,
+      valuesellcall: item.valuesellcall,
+      valuebuyput: item.valuebuyput,
+      valuesellput: item.valuesellput
+    }))
+    data.toppremiumcontracts = (
+      await query(`SELECT eventSymbol,rootSymbol,flag,expirydate, SUM(size),COUNT (*), SUM(price*size*100) as value,
+      SUM(case WHEN aggressorside = 'BUY' THEN price*size*100 ELSE 0 END) as buyvalue,
+      SUM(case WHEN aggressorside = 'SELL' THEN price*size*100 ELSE 0 END) as sellvalue
+      from tasc WHERE size > 0
+      GROUP BY eventSymbol,rootSymbol,flag,expirydate
+      ORDER BY SUM(price*size*100) DESC LIMIT 100`)
+    ).rows
+
+    data.toppremiumcontractsnospx = (
+      await query(`SELECT eventSymbol,rootSymbol,flag,expirydate, SUM(size),COUNT (*), SUM(price*size*100) as value,
+      SUM(case WHEN aggressorside = 'BUY' THEN price*size*100 ELSE 0 END) as buyvalue,
+      SUM(case WHEN aggressorside = 'SELL' THEN price*size*100 ELSE 0 END) as sellvalue
+      from tasc WHERE size > 0 AND rootSymbol != 'SPY' AND rootSymbol != 'SPX' AND rootSymbol != 'SPXW'
+      GROUP BY eventSymbol,rootSymbol,flag,expirydate
+      ORDER BY SUM(price*size*100) DESC LIMIT 100`)
+    ).rows
+
+    data.topvolmcontracts = (
+      await query(`SELECT eventSymbol,rootSymbol,flag,expirydate, SUM(size),COUNT (*), SUM(price*size*100) as value,
+      SUM(case WHEN aggressorside = 'BUY' THEN size ELSE 0 END) as buyvolm,
+      SUM(case WHEN aggressorside = 'SELL' THEN size ELSE 0 END) as sellvolm
+      from tasc WHERE size>0
+      GROUP BY eventSymbol,rootSymbol,flag,expirydate
+      ORDER BY SUM(size) DESC LIMIT 100`)
+    ).rows
+
+    data.topvolmcontractsnospx = (
+      await query(`SELECT eventSymbol,rootSymbol,flag,expirydate, SUM(size),COUNT (*), SUM(price*size*100) as value,
+      SUM(case WHEN aggressorside = 'BUY' THEN size ELSE 0 END) as buyvolm,
+      SUM(case WHEN aggressorside = 'SELL' THEN size ELSE 0 END) as sellvolm
+      from tasc WHERE size>0 AND rootSymbol != 'SPY' AND rootSymbol != 'SPX' AND rootSymbol != 'SPXW'
+      GROUP BY eventSymbol,rootSymbol,flag,expirydate
+      ORDER BY SUM(size) DESC LIMIT 100`)
+    ).rows
+
+    data.toptradespremium = (
+      await query(`SELECT *,(price*size*100) as value FROM tasc where size > 0
+      ORDER BY (price*size*100) DESC limit 100`)
+    ).rows
+
+    data.toptradespremiumnospx = (
+      await query(`SELECT *,(price*size*100) as value FROM tasc where size > 0 AND rootSymbol != 'SPY' AND rootSymbol != 'SPX' AND rootSymbol != 'SPXW'
+      ORDER BY (price*size*100) DESC limit 100`)
+    ).rows
+
+
+    data.stats = (
+      await query(`SELECT
+       sum(size*price*100) as value,
+       sum(size) as volm,
+       count(*) as count,
+       max(dayid)
+       from tasc where size>0`)
+    ).rows
+
+    console.log('got it all',new Date().getTime() - timeStart)
+
+    await query(`UPDATE jkeys set t_up=NOW(),body = '${JSON.stringify(data)}' where name='flowrep'`)
+
+
+    console.log('loaded it all and done',new Date().getTime() - timeStart)
+
+  } catch (e) {
+    console.error(e)
+  }
+};
+// getFlowRep()
+
+
+
+(async () =>  {
+  const contracts = await query(`SELECT rootsymbol,optioncode,openinterest - prevoi as oich from opt_db where optioncode != '';`)
+  console.log(contracts.rows.length)
+  console.log(contracts.rows[0])
+  var batch = client.batch();
+  for (var i = 0; i < contracts.rows.length; i++) {
+    batch.hmset(contracts.rows[i].optioncode,{
+      oich:contracts.rows[i].oich  || 0,
+      valuebuy:contracts.rows[i].valuebuy  || 0,
+      volmbuy:contracts.rows[i].volmbuy  || 0,
+      countbuy:contracts.rows[i].countbuy  || 0,
+      valuesell:contracts.rows[i].valuesell  || 0,
+      volmsell:contracts.rows[i].volmsell  || 0,
+      countsell:contracts.rows[i].countsell  || 0,
+      valueund:contracts.rows[i].valueund  || 0,
+      volmund:contracts.rows[i].volmund  || 0,
+      countund:contracts.rows[i].countund  || 0,
+      value:contracts.rows[i].value  || 0,
+      volm:contracts.rows[i].volm  || 0,
+      count:contracts.rows[i].count  || 0,
+      valuebs:contracts.rows[i].valuebs  || 0,
+      volmbs:contracts.rows[i].volmbs  || 0,
+      countbs:contracts.rows[i].countbs  || 0,
+      underlying:contracts.rows[i].rootsymbol
+    })
+  }
+  batch.exec((err, resp)=> {
+    if (err) {
+      console.log(err)
+      // reject(err)
+    }
+    console.log('batched')
+    // resolve()
+  });
+  console.log('done')
+})();
