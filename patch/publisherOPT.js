3cfce9834fcb40e21ea52b4dca30d4e0cb1d09b6
jbtobar <jbtobar.io@gmail.com>
2022-04-11 11:25:16 -0500

    cometOPT

---
+++ patch/publisherOPT.js
@@ -0,0 +1,253 @@
+/**
+ * Code written by and belongs to Juan Bernardo Tobar <jbtobar>
+ * jbtobar.io@gmail.com
+ * @author Juan Bernardo Tobar <jbtobar.io@gmail.com>
+ */
+const redis = require('redis');
+const client_redis = redis.createClient();
+const { query } = require('../db');
+
+const toggleChannel = (ch) => {
+    switch (ch) {
+        case 'cometOPT0':
+            return 'cometOPT1';
+        case 'cometOPT1':
+            return 'cometOPT2';
+        case 'cometOPT2':
+            return 'cometOPT3';
+        case 'cometOPT3':
+            return 'cometOPT4';
+        case 'cometOPT4':
+            return 'cometOPT5';
+        case 'cometOPT5':
+            return 'cometOPT0';
+        default:
+            console.log('foooooked')
+            return 'com'
+    }
+}
+
+function wait(milliseconds) {
+  return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+const timenow = () => new Date().toLocaleString("en-US", {timeZone: "America/New_York"})
+console.log('-----------------------------------------------------------------')
+console.log('-----------------------------------------------------------------')
+console.log('-----------------------------------------------------------------')
+console.log('-----------------------------------------------------------------')
+console.log('-----------------------------------------------------------------')
+console.log(`publisherOPT - start - ${timenow()} v4.55.56`)
+console.log('-----------------------------------------------------------------')
+console.log('-----------------------------------------------------------------')
+console.log('-----------------------------------------------------------------')
+console.log('-----------------------------------------------------------------')
+console.log('-----------------------------------------------------------------')
+
+const subbed = {}
+const subbbedUND = {}
+
+const subCommand = async () => {
+  console.log(`\n`)
+  console.log(`---------------------------------------------------------------`)
+  const timeStart = new Date().getTime();
+  console.log(`subbing ${timenow()}`)
+
+
+
+
+
+
+  let cometChannel = 'cometOPT0'
+
+
+
+  let codes = (
+    await query(`select optioncode,underlying_symbol,rootsymbol as underlying,replace(expirydate,'-','') as mmy,strike,expirydate as expiration from futchainx;`)
+  ).rows.filter(d => !subbed[d.optioncode])
+  console.log('step1',codes.length,codes[0])
+  while (codes.length > 0) {
+    const symbolLoad = codes.splice(0,2000)
+    symbolLoad.forEach((item, i) => {
+      subbed[item.optioncode] = true
+      item.underlying = item.underlying_symbol.slice(0, item.underlying_symbol.length-1) + '2' + item.underlying_symbol.slice(item.underlying_symbol.length-1) +":"+item.optioncode.split(':')[1]
+      client_redis.hmset(item.optioncode,{
+        ...item,
+        valuebuy:0,
+        volmbuy:0,
+        countbuy:0,
+        valuesell:0,
+        volmsell:0,
+        countsell:0,
+        valueund:0,
+        volmund:0,
+        countund:0,
+        value:0,
+        volm:0,
+        count:0,
+        valuebs:0,
+        volmbs:0,
+        countbs:0,
+        underlying:item.underlying_symbol.slice(0, item.underlying_symbol.length-1) + '2' + item.underlying_symbol.slice(item.underlying_symbol.length-1) +":"+item.optioncode.split(':')[1]
+      })
+    });
+    cometChannel = toggleChannel(cometChannel);
+    client_redis.publish(cometChannel,JSON.stringify({"add":{
+      "Greeks": symbolLoad.map(d => d.optioncode),
+      "Quote": symbolLoad.map(d => d.optioncode),
+      "Trade": symbolLoad.map(d => d.optioncode),
+      "Summary": symbolLoad.map(d => d.optioncode),
+    }}))
+    await wait(500);
+    // process.exit()
+  }
+
+
+  // for (var i = 0; i < optioncodes.length; i++) {
+  //   client_redis.hmset(optioncodes[i].symbol,{
+  //     underlying:optioncodes[i].underlying,
+  //     strike:optioncodes[i].strike,
+  //     expiration:optioncodes[i].expiration,
+  //     option_type:optioncodes[i].option_type,
+  //     mmy:optioncodes[i].mmy,
+  //   })
+  //   processed[optioncodes[i].symbol] = true;
+  // }
+  // console.log(futures,futs)
+  codes = [...new Set([
+    ...(await query(`select * from futuresdir`)).rows.map(d => ({name:d.name,symbol:d.dxsymbol})).map(d => d.symbol),
+    ...(await query(`select * from futdirx;`)).rows.map(d => ({
+      name:d.symbol,
+      symbol:d.symbol.slice(0, d.symbol.length-1) + "2" + d.symbol.slice(d.symbol.length-1)+':'+d.dxcode
+    })).map(d => d.symbol)
+  ])].filter(d => !subbed[d])
+  console.log('step2',codes.length,codes[0])
+
+  while (codes.length > 0) {
+    const symbolLoad = codes.splice(0,4000)
+    symbolLoad.forEach((item, i) => {
+      subbed[item] = true
+    });
+    cometChannel = toggleChannel(cometChannel);
+    client_redis.publish(cometChannel,JSON.stringify({"add":{
+      // "Greeks": symbolLoad,
+      "Quote": symbolLoad,
+      "Trade": symbolLoad,
+      "Summary": symbolLoad,
+      "Underlying": symbolLoad,
+      "Series": symbolLoad
+    }}))
+    await wait(500);
+    // process.exit()
+  }
+
+
+
+
+
+
+  // ---------------------------------------------------------------------------
+  // ---------------------------------------------------------------------------
+  // ---------------------------------------------------------------------------
+
+
+
+
+  codes = (
+    await query(`SELECT symbol as optioncode,underlying,strike,expiration,mmy,underlying,(substring(symbol FROM '(?<=\\d)[A-Z]{1}(?=\\d)')) as flag from ipf_opt;`)
+  ).rows.filter(d => d.optioncode && d.optioncode.length>0).filter(d => !subbed[d.optioncode])
+  console.log('step3',codes.length,codes[0])
+
+  let underlyings = [...new Set(codes.map(d => d.underlying))].filter(d => !subbed[d])
+  const commitTimeStart = new Date().getTime()
+  if (codes.length > 0) {
+    query (`INSERT into opt_db(optioncode,expirydate,strike,flag,rootsymbol)
+    (SELECT symbol as optioncode,expiration as expirydate,strike,(substring(symbol FROM '(?<=\\d)[A-Z]{1}(?=\\d)')) as flag,underlying as rootsymbol from ipf_opt l
+    where not exists (
+      select from opt_db where optioncode = l.symbol
+    ));`)
+    // SELECT symbol as optioncode,expiration as expirydate,strike,(substring(symbol FROM '(?<=\d)[A-Z]{1}(?=\d)')) as flag from ipf_opt
+  }
+  console.log(`commit finised - - ${new Date().getTime()-commitTimeStart}`)
+
+  while (codes.length > 0) {
+    const symbolLoad = codes.splice(0,2000)
+
+    // symbolLoad.forEach((item, i) => {
+    //   subbed[item.optioncode] = true
+    //   try {
+    //     client_redis.hmset(item.optioncode,{
+    //       ...item,
+    //       valuebuy:0,
+    //       volmbuy:0,
+    //       countbuy:0,
+    //       valuesell:0,
+    //       volmsell:0,
+    //       countsell:0,
+    //       valueund:0,
+    //       volmund:0,
+    //       countund:0,
+    //       value:0,
+    //       volm:0,
+    //       count:0,
+    //       valuebs:0,
+    //       volmbs:0,
+    //       countbs:0,
+    //     })
+    //   } catch (e) {
+    //     console.error(e,item)
+    //   }
+    // });
+    cometChannel = toggleChannel(cometChannel);
+    client_redis.publish(cometChannel,JSON.stringify({"add":{
+      "Greeks": symbolLoad.map(d => d.optioncode),
+      "Quote": symbolLoad.map(d => d.optioncode),
+      "Trade": symbolLoad.map(d => d.optioncode),
+      "Summary": symbolLoad.map(d => d.optioncode),
+    }}))
+    await wait(500);
+    // process.exit()
+  }
+  codes = [...underlyings]
+  underlyings = null
+
+  console.log('step4s',codes.length,codes[0])
+
+  // ---------------------------------------------------------------------------
+  // ---------------------------------------------------------------------------
+  // ---------------------------------------------------------------------------
+
+
+  while (codes.length > 0) {
+    const symbolLoad = codes.splice(0,2000)
+    symbolLoad.forEach((item, i) => {
+      subbed[item] = true
+    });
+
+    cometChannel = toggleChannel(cometChannel);
+    client_redis.publish(cometChannel,JSON.stringify({"add":{
+      "Quote": symbolLoad,
+      "Trade": symbolLoad,
+      "Summary": symbolLoad,
+      "Underlying": symbolLoad,
+      "Series": symbolLoad
+    }}))
+    await wait(500);
+    // process.exit()
+  }
+
+  // ---------------------------------------------------------------------------
+  // ---------------------------------------------------------------------------
+  // ---------------------------------------------------------------------------
+
+
+
+
+  console.log(`done - Duration: ${new Date().getTime() - timeStart}`)
+  console.log(`---------------------------------------------------------------`)
+  console.log(`\n`)
+}
+
+subCommand()
+setInterval(() => {
+  subCommand()
+},1000*60*10)
